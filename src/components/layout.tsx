199b30a777b2db8021a23d13e8c3832710a90fd6
kien mai <hasongva@gmail.com>
2022-09-06 11:22:30 +0700

    Init source

---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
